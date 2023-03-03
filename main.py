dd6eff3aa0ed7a92932a6924c7cf50d37da0034d
zzzolliom <zubolga419@gmail.com>
2023-03-03 14:09:09 +0300

    красный орнамент

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import turtle
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+t = turtle.Pen()
+t.pencolor("red")
+for x in range(50, 100):
+    t.forward(x)
+    t.left(92)
